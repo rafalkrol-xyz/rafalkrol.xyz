1f1d87df1751aadbffb839948187d3914fe5d8bb
Rafal Krol <ameotoko1+github@gmail.com>
2025-12-07 19:36:51 +0100

    Import the apex record

--- infra/route53.tf
+++ infra/route53.tf
@@ -1,3 +1,5 @@
+# TODO: move public hosted zone and amplify hosting to a production workload account // currently in the organization management account
+
 ### PUBLIC HOSTED ZONE - START
 resource "aws_route53_zone" "rafalkrol_xyz" {
   name = local.rafalkrol_xyz_phz
@@ -7,3 +9,17 @@ resource "aws_route53_zone" "rafalkrol_xyz" {
   }
 }
 ### PUBLIC HOSTED ZONE - END
+
+### DNS RECORDS - START
+# A Record - Apex domain to CloudFront distribution
+resource "aws_route53_record" "apex" {
+  zone_id = aws_route53_zone.rafalkrol_xyz.zone_id
+  name    = local.rafalkrol_xyz_phz
+  type    = "A"
+
+  alias {
+    name                   = "dmxdeoyuzd0lj.cloudfront.net" # TODO: import the amplify resource
+    zone_id                = "Z2FDTNDATAQYW2" # CloudFront hosted zone ID originating from an Amplify app # TODO: import the amplify resource
+    evaluate_target_health = false
+  }
+}
